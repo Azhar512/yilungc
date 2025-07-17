74b57b35470bdbb9c025aed42e586f30225dd660
Azhar Munir <azharsiddiqui8540@gmail.com>
2025-07-17 17:51:57 +0500

    update files

--- components/header.jsx
+++ components/header.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 import { useState, useEffect } from "react"
 import { Menu, XIcon, Search } from "lucide-react"
 import { usePathname } from "next/navigation"
-import { generateSlug } from "@/lib/utils"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import SearchOverlay from "@/components/search-overlay" // Import the new SearchOverlay
+import { generateSlug } from "../lib/utils"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../components/ui/dropdown-menu"
+import SearchOverlay from "../components/search-overlay" // Import the new SearchOverlay
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
