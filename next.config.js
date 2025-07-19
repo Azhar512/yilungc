96d8c0d99eb3672fe5d798744a550db8741cfd56
Azhar Munir <azharsiddiqui8540@gmail.com>
2025-07-19 17:11:51 +0500

    update files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc", // For your custom images
+      },
+      {
+        protocol: "https",
+        hostname: "www.notion.so", // For Notion's public URLs if any
+      },
+    ],
+    unoptimized: true,
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
